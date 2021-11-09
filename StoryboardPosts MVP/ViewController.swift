8f5548d66fe3d970b05e968730c2b8eaae30c058
Asadulla Juraev <tezkormedia@gmail.com>
2021-11-09 09:19:35 +0500

    Initial Commit

---
+++ StoryboardPosts MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardPosts MVP
+//
+//  Created by Asadulla Juraev on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
